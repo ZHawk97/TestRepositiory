a101239a05b41da65baa4d2b59f0ec8845920641
Zachary Hawkins <zhawk97@yahoo.com>
2014-09-18 13:04:07 -0500

    Add Addition method

--- src/test/test.java
+++ src/test/test.java
@@ -6,6 +6,16 @@ public class test {
 	{
 		System.out.println("Hello World");
 		System.out.println("Hey there i added this");
+		
+		System.out.println(add( 5 , 12 ));
+		
+	}
+	
+	public static int add( int one , int two )
+	{
+		
+		return one + two;
+		
 	}
 	
 }
